8de6d201316cdd5efdada935501b2271f6ed296c
MisterMaroki <omar987@hotmail.co.uk>
2022-03-23 23:59:13 +0000

    Generating password in slot-one

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const slotOne = document.getElementById("slot-one");
 const slotTwo = document.getElementById("slot-two");
 const slotThree = document.getElementById("slot-three");
 const slotFour = document.getElementById("slot-four");
+const slots = document.getElementsByClassName("slot");
+const chars =
+	"0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let generated = false;
+let passwordLength = 9;
+
+generateBtn.addEventListener("click", function () {
+	generated = true;
+	var password = "";
+	var passwordTwo = "";
+	var passwordThree = "";
+	var passwordFour = "";
+
+	for (let i = 0; i <= passwordLength; i++) {
+		let randomNumber = Math.floor(Math.random() * chars.length);
+		password += chars.substring(randomNumber, randomNumber + 1);
+	}
+	slotOne.textContent = password;
+});
