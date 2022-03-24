6e232ac1ec7cce4d6152ee4fb8cdb2bb6399bf5a
MisterMaroki <omar987@hotmail.co.uk>
2022-03-24 01:43:13 +0000

    no need for a substring

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ generateBtn.addEventListener("click", function () {
 
 	function genPass() {
 		let password = "";
-		for (let i = 0; i <= passwordLength; i++) {
+		for (let i = 0; i < passwordLength; i++) {
 			let randomNumber = Math.floor(Math.random() * chars.length);
-			password += chars.substring(randomNumber, randomNumber + 1);
+			password += chars[randomNumber];
 		}
 		return password;
 	}
