8f6d5944fdcc410f8ea161f35188afff1426e717
MisterMaroki <omar987@hotmail.co.uk>
2022-03-24 13:21:29 +0000

    to add copy functionality, slots need to be strings

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ function genPass() {
 	let password = "";
 	for (let i = 0; i < passwordLength; i++) {
 		let randomNumber = Math.floor(Math.random() * chars.length);
-		password += chars[randomNumber];
+		password += chars.substring(randomNumber, randomNumber + 1);
 	}
 	return password;
 }
