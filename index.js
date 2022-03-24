4ee6cd16661314511d629a6675e0f9c8f06655e6
MisterMaroki <omar987@hotmail.co.uk>
2022-03-24 00:03:11 +0000

    changed to function to avoid repeating

--- index.js
+++ index.js
@@ -15,10 +15,12 @@ generateBtn.addEventListener("click", function () {
 	var passwordTwo = "";
 	var passwordThree = "";
 	var passwordFour = "";
-
-	for (let i = 0; i <= passwordLength; i++) {
-		let randomNumber = Math.floor(Math.random() * chars.length);
-		password += chars.substring(randomNumber, randomNumber + 1);
+	function genPass() {
+		for (let i = 0; i <= passwordLength; i++) {
+			let randomNumber = Math.floor(Math.random() * chars.length);
+			password += chars.substring(randomNumber, randomNumber + 1);
+		}
+		return password;
 	}
-	slotOne.textContent = password;
+	slotOne.textContent = genPass();
 });
